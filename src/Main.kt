950b4673e9cf99dc733292b0ab532c41e1ca67f8
10231378 <evgeniakalasnikova489@gmail.co>
2024-09-09 20:24:52 +0300

    добавила строку

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,28 @@
 fun main() {
-    println("Hello World!")
-    println("Hello World!")
+    var valstring = "AAADSSSRRTTHAAAA"
+    var countchar = 1;
+    var prevChar = valstring[0];
+    var result = ""
+    for (i in 1 .. valstring.length -1) {
+        if(prevChar == valstring[i]){
+            countchar ++
+        } else {
+            if (countchar > 1) {
+                result = result + prevChar + "$countchar"
+            }
+            else {
+                result =result + prevChar
+            }
+            countchar =1
+            prevChar = valstring[i]
+        }
+    }
+    if (countchar > 1) {
+        result = result + prevChar + "$countchar"
+    }
+    else {
+        result =result + prevChar
+    }
+
+    println(result)
 }
